b6387d3061443734c35ebdc7b0a967da13380730
v5.0 MYSTIC <mm0651225@gmail.com>
2025-09-07 00:43:48 +0300

    new changes

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify1/core/configs/assets/app_images.dart';
 import 'package:spotify1/core/configs/assets/app_vectors.dart';
+import 'package:spotify1/core/configs/theme/app_colors.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -12,6 +13,7 @@ class GetStartedPage extends StatelessWidget {
       body: Stack(
         children: [
           Container(
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
             decoration: const BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.fill,
@@ -24,6 +26,7 @@ class GetStartedPage extends StatelessWidget {
                   alignment: Alignment.topCenter,
                   child: SvgPicture.asset(
                     AppVectors.Logo,
+                    height: 70,
                   ),
                 ),
                 Spacer(),
@@ -36,6 +39,17 @@ class GetStartedPage extends StatelessWidget {
                       
                     ),
                   ),
+                  SizedBox(height: 21,),
+                  Text (
+                    'Chasing dreams through late nights and city lights, I turn my struggles into verses. Every beat is a step, every rhyme a story — this is more than music, it’s my way of life.',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.grey,
+                      fontSize: 14, 
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+
                 
               ],
             ),
